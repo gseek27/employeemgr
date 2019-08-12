7b8d5af082ac9ea1eb1debba80c2486a7606c7f2
gseek27 <gosalseek27@gmail.com>
2019-08-11 19:18:09 -0700

    uploaded employeemgr proj

---
+++ src/components/newemployee.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="new-employee">
+        <h3>New Employeee
+            </h3>
+        </div>
+</template>
+
+<script>
+export default {
+    name: 'new-employee',
+    data ()  {
+        return {
+            
+        }
+    }
+    
+}
+</script>
